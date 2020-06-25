74d3bcffe5473121bc285f8dae3dbfef33d3d067
wonderf <pobolinstas@gmail.com>
2020-06-25 23:57:03 +0500

    demo save svg with name

--- index.js
+++ index.js
@@ -1,8 +1,57 @@
 var express = require('express');
-var app = express();
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const _ = require('lodash');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var app = express();
+app.use(fileUpload({
+    createParentPath: true
+}));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/upload-svg', async (req, res) => {
+    try {
+        if(!req.files) {
+            res.send({
+                status: false,
+                message: 'No file uploaded'
+            });
+        } else {
+            const regex_name=/(_[x]+.svg)/;
+            const svg = req.files.svg;
+            const start = req.body.start;
+            const end = req.body.end;
+            const base_name = svg.name.split(regex_name)[0];
+            console.log(base_name);
+            for(let i=start;i<=end;i++){
+                svg.mv('./uploads/' + base_name+"_"+i+".svg");
+            }
+            
+            
+
+            //send response
+            res.send({
+                status: true,
+                message: 'File is uploaded',
+                data: {
+                    name: svg.name,
+                    mimetype: svg.mimetype,
+                    size: svg.size
+                }
+            });
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
 });
 
 app.listen(3000, function () {
