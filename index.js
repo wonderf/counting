0959eafca13a64877391390e1288c51ee86a7eb4
wonderf <pobolinstas@gmail.com>
2020-06-26 00:48:41 +0500

    complete first part

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const _ = require('lodash');
+const zip = require('express-easy-zip')
+const rimraf = require("rimraf");
 
 var app = express();
 app.use(fileUpload({
     createParentPath: true
 }));
 app.use(cors());
+app.use(zip());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
@@ -30,6 +33,7 @@ app.post('/upload-svg', async (req, res) => {
             const svg = req.files.svg;
             const start = req.body.start;
             const end = req.body.end;
+            const dirPath = __dirname + "/uploads";
             const base_name = svg.name.split(regex_name)[0];
             console.log(base_name);
             for(let i=start;i<=end;i++){
@@ -39,20 +43,21 @@ app.post('/upload-svg', async (req, res) => {
             
 
             //send response
-            res.send({
-                status: true,
-                message: 'File is uploaded',
-                data: {
-                    name: svg.name,
-                    mimetype: svg.mimetype,
-                    size: svg.size
-                }
+            await res.zip({
+                files: [{
+                    path: dirPath,
+                    name: 'files'
+                }],
+                filename: 'files.zip'
             });
+            rimraf(dirPath, function () { console.log("done"); });
+
         }
     } catch (err) {
         res.status(500).send(err);
     }
 });
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
